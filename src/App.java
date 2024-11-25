a483bf03639abea628c3a2f8964310ddbbfe60d2
RafaelBSoul <coffeemonstersandbooks@gmail.com>
2024-11-25 07:34:57 -0300

    choose

--- src/App.java
+++ src/App.java
@@ -2,12 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Montros.Monstros;
+import Personagens.Personagens;
 
 public class App {
     public static void main(String[] args) throws Exception {
     List<Monstros> pokemon = new ArrayList<>();
+    List<Personagens> treinador = new ArrayList<>();
+
+    Personagens treiner = new Personagens();
     Monstros no1 = new Monstros();
     Monstros no2 = new Monstros();
+
+    treiner.setNome("Spark");
+    treiner.setAtaque(2);
+    treiner.setDefesa(3);
 
     no1.setNomeMonstros("javai");
     no1.setDefesa(12);
@@ -17,7 +25,9 @@ public class App {
     no2.setForca(7);
     pokemon.add(no1);
     pokemon.add(no2);
+    System.out.println(treiner);
     System.out.println(pokemon);
+
 
 
 
--- src/Personagens/Personagens.java
+++ src/Personagens/Personagens.java
@@ -5,5 +5,34 @@ public class Personagens {
     int ataque;
     int defesa;
     int agilidades;
+    @Override
+    public String toString() {
+        return "Personagens [getNome()=" + getNome() + ", getAtaque()=" + getAtaque() + ", getDefesa()=" + getDefesa()
+                + ", getAgilidades()=" + getAgilidades() + "]";
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getAtaque() {
+        return ataque;
+    }
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+    public int getDefesa() {
+        return defesa;
+    }
+    public void setDefesa(int defesa) {
+        this.defesa = defesa;
+    }
+    public int getAgilidades() {
+        return agilidades;
+    }
+    public void setAgilidades(int agilidades) {
+        this.agilidades = agilidades;
+    }
     
 }
